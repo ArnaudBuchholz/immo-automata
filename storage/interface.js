c44bf64ef3ef3c08d1407983fcfc127a87ddf946
Arnaud Buchholz <arnaud.buchholz@free.fr>
2016-05-08 11:14:49 -0400

    Storage interface

---
+++ storage/interface.js
@@ -0,0 +1,53 @@
+"use strict";
+
+function _ignore() {}
+
+module.exports = {
+
+    /**
+     * Initialize storage based on provided configuration
+     *
+     * @param {Object} config Storage specific config
+     * @returns {Promise}
+     */
+    init: function (config) {
+        _ignore(config);
+        return Promise.resolve();
+    },
+
+    /**
+     * Retrieve a record from the storage
+     *
+     * @param {String} uid Record unique id
+     * @return {Promise<Object|nul>}
+     */
+    find: function (uid) {
+        _ignore(uid);
+        return Promise.resolve(null);
+    },
+
+    /**
+     * Adds a record to the storage
+     *
+     * @param {String} uid Record unique id
+     * @param {Object} record Record values
+     * @param {Boolean} updated Record was updated (or is new if false)
+     * @return {Promise}
+     */
+    set: function (uid, record, updated) {
+        _ignore(uid);
+        _ignore(record);
+        _ignore(updated);
+        return Promise.resolve(null);
+    },
+
+    /**
+     * Terminate the storage
+     *
+     * @returns {Promise}
+     */
+    close: function () {
+        return Promise.resolve();
+    }
+
+};
