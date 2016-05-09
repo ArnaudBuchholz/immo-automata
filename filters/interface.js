947de9538abb972d15b017fa6ede2f046e5fd29d
Arnaud Buchholz <arnaud.buchholz@free.fr>
2016-05-08 23:53:54 -0400

    Only one method required

--- filters/interface.js
+++ filters/interface.js
@@ -5,27 +5,18 @@ function _ignore() {}
 module.exports = {
 
     /**
-     * Initialize filter based on provided configuration
-     *
-     * @param {Object} config Filter specific config
-     * @returns {Promise}
-     */
-    init: function (config) {
-        _ignore(config);
-        return Promise.resolve();
-    },
-
-    /**
      * Filter the record.
      * The resolved value might be either:
      * - The original record itself
      * - A modified record
      * - null (the record is ignored)
      *
+     * @param {Object} config Filter specific config
      * @param {Object} record
      * @returns {Promise<Object|null>}
      */
-    process: function (record) {
+    filter: function (config, record) {
+        _ignore(config);
         return Promise.resolve(record);
     }
 
