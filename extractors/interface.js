a50d4e4cfb50fac929f12ee9e336ded984a61e90
Arnaud Buchholz <arnaud.buchholz@free.fr>
2016-05-08 23:53:37 -0400

    Only one method required

--- extractors/interface.js
+++ extractors/interface.js
@@ -5,17 +5,18 @@ function _ignore() {}
 module.exports = {
 
     /**
-     * Initialize and start extraction.
+     * Configure and start extraction.
      * For each extracted record you want to keep, the callback must be used.
      * The extractor must wait for the callback to resolve to continue its work.
+     * If the callback promise is rejected, the extractor must stop.
      *
      * @param {Object} config Extractor specific config
      * @param {Function} callback:
-     *  function (record) { return Promise.resolve(); }
+     *  function (uid, record) { return Promise.resolve(); }
      *
      * @returns {Promise}
      */
-    init: function (config, callback) {
+    start: function (config, callback) {
         _ignore(config);
         _ignore(callback);
         return Promise.resolve();
