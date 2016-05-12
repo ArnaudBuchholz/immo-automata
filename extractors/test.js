0ecf255e325810d9ce014050bee306982657e273
Arnaud Buchholz <arnaud.buchholz@free.fr>
2016-05-12 19:14:44 -0400

    Documentation

--- extractors/test.js
+++ extractors/test.js
@@ -1,31 +1,43 @@
 "use strict";
 
+var CONSTANTS = require("../constants.js"),
+    helpers = require("../helpers.js");
+
 module.exports = {
 
+    /**
+     * @inheritdoc extractors#start
+     *
+     * Simulates extraction by enumerating items array from the configuration.
+     * A timeout can be specified to slow down the process
+     */
     start: function (config, callback) {
         var idx = 0,
             done,
-            failed,
-            result = new Promise(function (resolve, reject) {
+            promise = new Promise(function (resolve) {
                 done = resolve;
-                failed = reject;
             });
+
         function loop () {
             setTimeout(function () {
-                callback(config.id + "." + idx, config.items[idx])
+                var record = {};
+                record[CONSTANTS.RECORD_UID] = config.id + "." + idx;
+                helpers.extend(record, config.items[idx]);
+                callback(record)
                     .then(function () {
                         if (++idx < config.items.length) {
                             loop();
                         } else {
-                            done ();
+                            done();
                         }
                     }, function () {
                         done();
                     });
             }, config.timeout || 0);
         }
+
         loop();
-        return result;
+        return promise;
     }
 
 };
