0259d4276e9b6b2f82214378306bd7355237000e
Arnaud Buchholz <arnaud.buchholz@free.fr>
2016-05-12 19:14:57 -0400

    Match new interface

--- filters/test.js
+++ filters/test.js
@@ -1,30 +1,28 @@
 "use strict";
 
-function _extend (obj, members) {
-    for (var name in members) {
-        if (members.hasOwnProperty(name)) {
-            obj[name] = members[name];
-        }
-    }
-}
+var // CONSTANTS = require("../constants.js"),
+    helpers = require("../helpers.js");
 
 module.exports = {
 
     filter: function (config, record) {
-        return new Promise(function (resolve, reject) {
+        return new Promise(function (resolve/*, reject*/) {
             var result = record;
+
+            // Filter out based on label matching with exclude array
             if (config.exclude && -1 < config.exclude.indexOf(record.label)) {
                 result = null;
+
+            // Adds dynamic properties
+            } else if (config.add) {
+                result = helpers.extend({}, record, config.add);
+                result.isVoyel = -1 < "aeiouy".indexOf(record.label);
             }
-            if (result && config.add) {
-                result = {};
-                _extend(result, record);
-                _extend(result, config.add);
-            }
+
             setTimeout(function () {
                 resolve(result);
             }, config.timeout || 0);
-        })
+        });
     }
 
 };
