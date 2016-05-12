fd8edc4f8abe66c7f1f935cbf5e0347a25463722
Arnaud Buchholz <arnaud.buchholz@free.fr>
2016-05-12 19:14:33 -0400

    Documentation

--- filters/interface.js
+++ filters/interface.js
@@ -1,24 +1,24 @@
+/*eslint-disable no-unused-vars*/
 "use strict";
-
-function _ignore() {}
 
 module.exports = {
 
     /**
-     * Filter the record.
-     * The resolved value might be either:
-     * - The original record itself
-     * - A modified record
+     * Filter record.
+     *
+     * This method is triggered with the filter configuration *and* the record.
+     * The resolved record might be either:
+     * - The original record itself (if untouched)
+     * - A new record (if modified), the property require("../constants.js").RECORD_UID must be copied
      * - null (the record is ignored)
      *
-     * An empty object is allocated and used as a this context for the filter.
+     * An empty object is allocated and used as a this context for the filter (one per config).
      *
      * @param {Object} config Filter specific config
-     * @param {Object} record
-     * @returns {Promise<Object|null>}
+     * @param {Object} record Record to process
+     * @returns {Promise<Object|null>} A promise resolving to the filtered record
      */
     filter: function (config, record) {
-        _ignore(config);
         return Promise.resolve(record);
     }
 
