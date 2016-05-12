abcea81809334aab1a5da5a6bfda3e89df7e45a2
Arnaud Buchholz <arnaud.buchholz@free.fr>
2016-05-12 19:14:30 -0400

    Documentation

--- extractors/interface.js
+++ extractors/interface.js
@@ -1,26 +1,27 @@
+/*eslint-disable no-unused-vars*/
 "use strict";
-
-function _ignore() {}
 
 module.exports = {
 
     /**
      * Configure and start extraction.
-     * For each extracted record you want to keep, the callback must be used.
-     * The extractor must wait for the callback to resolve to continue its work.
-     * If the callback promise is rejected, the extractor must stop.
      *
-     * An empty object is allocated and used as a this context for the filter.
+     * For each extracted record that should be processed, the callback must be used.
+     * Records must have a member with the name require("../constants.js").RECORD_UID
+     *
+     * The callback returns a promise:
+     * - The extractor must wait for the promise to resolve to continue its work
+     * - If the promise is rejected, the extractor must stop
+     *
+     * An empty object is allocated and used as a this context for the extractor (one per config).
      *
      * @param {Object} config Extractor specific config
      * @param {Function} callback:
      *  function (record) { return Promise.resolve(); }
      *
-     * @returns {Promise}
+     * @returns {Promise} A promise resolved when the extractor ends
      */
     start: function (config, callback) {
-        _ignore(config);
-        _ignore(callback);
         return Promise.resolve();
     }
 
