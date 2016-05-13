374e717473dd858f3a16cf5fe554aec5523465e1
Arnaud Buchholz <arnaud.buchholz@free.fr>
2016-05-12 23:06:34 -0400

    Object comparison helper

--- helpers.js
+++ helpers.js
@@ -1,24 +1,36 @@
-(function () {
-    "use strict";
+"use strict";
 
-    module.exports = {
+var deepEqual = require("deep-equal");
 
-        /**
-         * Extend the object with the members of provided dictionaries
-         *
-         * @param {Object} obj receiving object
-         * @param {Object} ..members additional members dictionaries
-         * @returns {Object} the object
-         */
-        extend: function (obj) {
-            [].slice.call(arguments, 1).forEach(function (members) {
-                Object.keys(members).forEach(function (name) {
-                    obj[name] = members[name];
-                });
+module.exports = {
+
+    /**
+     * Extend the object with the members of provided dictionaries.
+     *
+     * @param {Object} obj receiving object
+     * @param {Object} ..members additional members dictionaries
+     * @returns {Object} the object
+     */
+    extend: function (obj) {
+        [].slice.call(arguments, 1).forEach(function (members) {
+            Object.keys(members).forEach(function (name) {
+                obj[name] = members[name];
             });
-            return obj;
-        }
+        });
+        return obj;
+    },
 
-    };
+    /**
+     * Deep compare objects.
+     *
+     * @param {Object} obj1
+     * @param {Object} obj2
+     * @returns {Boolean} The objects are equal
+     */
+    equal: function (obj1, obj2) {
+        return deepEqual(obj1, obj2, {
+            strict: true
+        });
+    }
 
-}());
+};
