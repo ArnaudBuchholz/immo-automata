057b56f62552d5934b35badc5d4ec430bbd887f3
Arnaud Buchholz <arnaud.buchholz@free.fr>
2016-05-18 23:46:21 -0400

    Recursive extend & Ordered column mapping

--- helpers.js
+++ helpers.js
@@ -6,15 +6,22 @@ module.exports = {
 
     /**
      * Extend the object with the members of provided dictionaries.
+     * Allows recursive extension of objects.
      *
      * @param {Object} obj receiving object
      * @param {Object} ..members additional members dictionaries
      * @returns {Object} the object
      */
-    extend: function (obj) {
+    extend: function _extend (obj) {
         [].slice.call(arguments, 1).forEach(function (members) {
             Object.keys(members).forEach(function (name) {
-                obj[name] = members[name];
+                var source = obj[name],
+                    overwrite = members[name];
+                if ("object" === typeof source && "object" === typeof overwrite) {
+                    obj[name] = _extend(source, overwrite);
+                } else {
+                    obj[name] = overwrite;
+                }
             });
         });
         return obj;
@@ -54,23 +61,45 @@ module.exports = {
     },
 
     /**
-     * Used for CSV import: build new columns on the record by applying mapping dictionary
+     * Used for CSV import: build new columns on the record by applying mapping dictionary.
+     * Columns can be order using a numeric prefix, for instance 99:TYPE
+     * This allows using the initial value TYPE and then replace it with a new value
      *
      * @param {Object} record Record to update
      * @param {Object} mapping Field definition dictionary
      * @returns {Object} record
      */
     mapColumns: function (record, mapping) {
-        Object.keys(mapping).forEach(function (key) {
-            var value = mapping[key];
-            Object.keys(record).forEach(function (member) {
-                var search = "{" + member + "}";
-                if (-1 < value.indexOf(search)) {
-                    value = value.split(search).join(record[member]);
+        Object.keys(mapping)
+            .map(function (column) {
+                var pos = column.indexOf(":");
+                if (-1 < pos) {
+                    return {
+                        id: column,
+                        order: parseInt(column.substr(0, pos), 10),
+                        name: column.substr(pos + 1)
+                    };
                 }
+                return {
+                    id: column,
+                    order: 0,
+                    name: column
+                };
+            })
+            .sort(function (a, b) {
+                return a.order - b.order;
+            })
+            .forEach(function (columnDef) {
+                var name = columnDef.name,
+                    value = mapping[columnDef.id];
+                Object.keys(record).forEach(function (member) {
+                    var search = "{" + member + "}";
+                    if (-1 < value.indexOf(search)) {
+                        value = value.split(search).join(record[member]);
+                    }
+                });
+                record[name] = value;
             });
-            record[key] = value;
-        });
     },
 
     /**
